8926883bb16c0c707c1e88f66cce72b166644343
kritika <kritikajain6104@gmail.com>
2025-06-21 23:00:41 +0530

    Added 1D Array Model of Rubiks Cube

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace  std;
 
+#include "Model/RubiksCube1dArray.cpp"
 #include "Model/RubiksCube3dArray.cpp"
+
 int main() {
     // RubiksCube3dArray object3DArray;
     //
@@ -18,6 +20,15 @@ int main() {
     //
     // if (object3DArray.isSolved()) cout << "SOLVED\n\n";
     // else cout << "NOT SOLVED\n\n";
+
+
+    RubiksCube1dArray object1dArray;
+
+    object1dArray.print();
+    object1dArray.u();
+    object1dArray.print();
+    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
+    else cout << "NOT SOLVED\n\n";
 
     return 0;
 }
