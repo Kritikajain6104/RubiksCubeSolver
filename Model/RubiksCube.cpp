b8ceb7b0500be39b6987431624f826d56bc67c0d
kritika <kritikajain6104@gmail.com>
2025-06-21 15:50:29 +0530

    Added Generic Class for Rubiks Cube

---
+++ Model/RubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kriti on 21-06-2025.
+//
+
+#include "RubiksCube.h"
---
+++ Model/RubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by kriti on 21-06-2025.
+//
+
+#ifndef RUBIKSCUBE_H
+#define RUBIKSCUBE_H
+
+
+
+class RubiksCube {
+
+};
+
+
+
+#endif //RUBIKSCUBE_H
